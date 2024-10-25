d9e5dc56ef2433a0bdf9cd9e8994c0e30ec7aebf
FranChesK0 <khl_doss@mail.ru>
2024-10-25 20:30:29 +0800

    fix config port type

--- game-of-life/core/cfg.py
+++ game-of-life/core/cfg.py
@@ -1,13 +1,24 @@
 import os
 import sys
+from typing import Optional
 
 from pydantic_settings import BaseSettings
+
+
+def load_port() -> Optional[int]:
+    port = os.getenv("GAME_OF_LIFE_PORT")
+    if port is None:
+        return port
+    try:
+        return int(port)
+    except ValueError:
+        return None
 
 
 class Config(BaseSettings):
     debug: bool = "-d" in sys.argv or "--debug" in sys.argv
     addr: str = os.getenv("GAME_OF_LIFE_ADDR") or "0.0.0.0"
-    port: str = os.getenv("GAME_OF_LIFE_PORT") or "3000"
+    port: int = load_port() or 3000
 
 
 config = Config()
