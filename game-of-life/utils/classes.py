e26507495f29c869b9d88be98272be2c7158310f
FranChesK0 <khl_doss@mail.ru>
2024-10-25 19:38:26 +0800

    fix singleton meta class

--- game-of-life/utils/classes.py
+++ game-of-life/utils/classes.py
@@ -3,13 +3,12 @@ from threading import Lock
 
 
 class SingletonMeta(type):
-    _instance: Dict[type["SingletonMeta"], "SingletonMeta"] = {}
+    _instance: Dict["SingletonMeta", "SingletonMeta"] = {}
     _lock: Lock = Lock()
 
-    @classmethod
-    def __call__(cls, *args: Any, **kwds: Any) -> "SingletonMeta":
-        with cls._lock:
-            if cls not in cls._instance or args or kwds:
+    def __call__(self, *args: Any, **kwds: Any) -> "SingletonMeta":
+        with self._lock:
+            if self not in self._instance or args or kwds:
                 instance = super().__call__(*args, **kwds)
-                cls._instance[cls] = instance
-        return cls._instance[cls]
+                self._instance[self] = instance
+        return self._instance[self]
