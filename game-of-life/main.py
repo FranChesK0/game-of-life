153381fd4b6d340d94b3f92c6faba92a799fe58a
FranChesK0 <khl_doss@mail.ru>
2024-10-25 19:28:39 +0800

    add game to functions

--- game-of-life/main.py
+++ game-of-life/main.py
@@ -1,16 +1,21 @@
 from flask import Flask, render_template
 
+from game import GameOfLife
+
 app = Flask(__name__)
 
 
 @app.route("/")
 def index() -> str:
+    GameOfLife(25, 25)
     return render_template("index.html")
 
 
 @app.route("/live")
 def live() -> str:
-    return render_template("live.html")
+    game = GameOfLife()
+    game.form_new_generation()
+    return render_template("live.html", game=game)
 
 
 if __name__ == "__main__":
